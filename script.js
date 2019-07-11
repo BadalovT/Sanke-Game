59697cdca69a9da6e66216ebcd763a704b97aeea
b.tebriz <pronet.pro12@pronet.az>
2019-07-11 17:30:17 +0400

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 var snakeWidth=10;
 var snakeHeight=10;
-var snakePositionLeft=10;
-var snakePositionUp=10;
+var snakePositionLeft=0;
+var snakePositionUp=0;
+var foodPositionLeft=Math.round(Math.random()/10*790)/10*100;
+var foodPositionTop=Math.round(Math.random()/10*390)/10*100;
 var right=document.querySelector(".right")
 var left=document.querySelector(".left")
 var up=document.querySelector(".up")
 var down=document.querySelector(".down")
+
+var isright=true;
+var isleft=true;
+var isdown=true;
+var isup=true;
 
 
 
@@ -15,20 +22,38 @@ var createSnake = function(){
   document.getElementById("root").appendChild(element);
 }
 
+var createFood = function(){
+  var element=document.createElement("div");
+  element.classList.add("food");
+  element.style.left = foodPositionLeft+"px";
+  element.style.top = foodPositionTop+"px";
+
+  document.getElementById("root").appendChild(element);
+}
+
 
 document.onkeydown = function(e) {
   switch (e.keyCode) {
       case 37:
+        if (isleft==true) {
           leftPress()
+        }
           break;
       case 38:
+        if (isup==true) {
          upPress()
+        }
           break;
       case 39:
+        if (isright==true) {
           rightPress()
+          
+        }
           break;
       case 40:
+        if (isdown=true) {
           downpress()
+        }
           break;
   }
 };
@@ -39,12 +64,18 @@ var rightPress = function(){
     snakePositionLeft+=10;
   }
   document.querySelector(".snake").style.left = snakePositionLeft+"px";
+  isleft=false;
+  isdown==true;
+  isup=true;
 }
 var leftPress= function(){
   if (snakePositionLeft>0) {
     snakePositionLeft-=10;
   }
   document.querySelector(".snake").style.left = snakePositionLeft+"px";
+  isright=false;
+  isdown==true;
+  isup=true;
 }
 
 var upPress= function(){
@@ -52,6 +83,9 @@ var upPress= function(){
     snakePositionUp-=10;
   }
   document.querySelector(".snake").style.top = snakePositionUp+"px";
+  isdown=false;
+  isleft=true;
+  isright=true;
 }
 
 var downpress= function(){
@@ -59,6 +93,10 @@ var downpress= function(){
     snakePositionUp+=10;
   }
   document.querySelector(".snake").style.top = snakePositionUp+"px";
+  isup=false;
+  isleft=true;
+  isright=true;
 }
 
 createSnake();
+createFood();
